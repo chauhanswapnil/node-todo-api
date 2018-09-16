c316550d6c6c0c54406560ffd317df1366b853b7
Swapnil Chauhan <swapnilchauhan@ymail.com>
2018-09-16 21:30:27 +0530

    Added DELETE ROUTE AND TESTS

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
+var uri = "mongodb://swapstar:M0U20Skqnwdwpe7F@ds121382.mlab.com:21382/todoappswapstar"
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true });
+mongoose.connect(uri || 'mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true });
 
 module.exports = {
   mongoose
