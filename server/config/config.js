7cc45ebea1d7dd4709a2095eca18f8e5a5d5d873
Swapnil Chauhan <swapnilchauhan@ymail.com>
2018-09-17 19:33:54 +0530

    Made some changes. Deployment Ready

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,17 @@
-var env = process.env.NODE_ENV || 'development';
-console.log(env);
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-}
+// var env = process.env.NODE_ENV;
+// if (env)
+// {
+//   return;
+// }
+// else{
+//     env = 'development';
+// }
+//
+// console.log(env);
+// if (env === 'development') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+// } else if (env === 'test') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+// }
